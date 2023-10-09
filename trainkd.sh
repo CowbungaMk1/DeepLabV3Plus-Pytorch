29a98d61b2a74554e08d4409b2d47338602685a4
CowbungaMk1 <83180720+CowbungaMk1@users.noreply.github.com>
2023-10-09 15:16:44 -0500

    addKDstuff

---
+++ trainkd.sh
@@ -0,0 +1,1 @@
+python3 main.py --model deeplabv3plus_mobilenet --dataset cityscapes  --gpu_id 0  --lr 0.1  --crop_size 768 --batch_size 2 --output_stride 8 --data_root /media/floyd/4TB_Mobile/datasets/cityscapes 
